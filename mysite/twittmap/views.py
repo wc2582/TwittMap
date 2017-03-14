475c0a0ac50d89684624b9945eff85a9b9470b31
Weihe Chen <wc2582@columbia.edu>
2017-03-13 22:15:00 -0400

    ajax test

--- mysite/twittmap/views.py
+++ mysite/twittmap/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 # Create your views here.
 from .models import Tweet
@@ -15,10 +16,31 @@ def index(request):
 		"query": {
 			"match_all":{}
 			},
-		"size": 1000
+		"size": 1
 		#"sort": [{"time": {"order": "desc"}}]
 		}
 	search_addr = '%s/twittmap/tweets/_search' % (HOST)
 	response = requests.post(search_addr, data = json.dumps(req))
 	print (response.text)
 	return render(request, 'twittmap/index.html')
+
+def search(request):
+    #searchtext = request.POST['searchtext']
+    searchtext = request.POST.get('searchtext', False)
+    print (searchtext)
+    #response = '<h1>test<h1>'
+
+
+    req = {
+    	"query": {
+    		"match_all":{}
+    		},
+    	"size": 1
+    	#"sort": [{"time": {"order": "desc"}}]
+    	}
+    search_addr = '%s/twittmap/tweets/_search' % (HOST)
+    response = requests.post(search_addr, data = json.dumps(req))
+
+
+    return HttpResponse(response)
+    #return HttpResponse(response, content_type='application/json')
