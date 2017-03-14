840585f7491c26c0d3c7ef5d27e4e5741f6a5b58
Weihe Chen <wc2582@columbia.edu>
2017-03-14 12:32:23 -0400

    minor correction

--- mysite/twittmap/urls.py
+++ mysite/twittmap/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^ajax/search/', views.search, name='search')
+    url(r'^ajax/search/', views.search, name='search'),
     url(r'^ajax/update/', views.update, name='update')
 ]
